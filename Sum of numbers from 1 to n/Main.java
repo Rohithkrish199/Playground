4e82d5aa7684787e53c071e9d0d1924ff7c6f1c4
Rohithkrish199 <34683089+Rohithkrish199@users.noreply.github.com>
2019-02-14 23:36:39 +0530

    Test code pushed to repository.

---
+++ Sum of numbers from 1 to n/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main{
+	public static void main (String[] args){
+      int n;
+      Scanner in=new Scanner(System.in);
+      n=in.nextInt();
+      int num=(n*(n+1))/2;
+      System.out.println(num);
+	     // Type your code here
+	}
+}
